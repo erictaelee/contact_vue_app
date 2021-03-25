ee419255e8f55cd455ce90d161d32b092fcac500
Eric <lee.erictae@gmail.com>
2021-03-24 21:07:21 -0500

    init, add index method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h3>Create a Contact</h3>
+    <p>
+      first name:
+      <input type="text" v-model="newContactFirstName" />
+    </p>
+    <p>
+      last name:
+      <input type="text" v-model="newContactLastName" />
+      </p>
+    <p>
+      email:
+      <input type="text" v-model="newContactEmail" />
+    </p>
+    <p>
+      phone number:
+      <input type="text" v-model="newContactPhoneNumber" />
+    </p>
+    <p>
+      image url:
+      <input type="text" v-model="newContactImage" />
+    </p>
+    <button v-on:click="contactsCreate">Create new Contact</button>
+
+
+    <div v-for="contact in contacts">
+     <p> {{ contact.first_name }} </p>
+     <hr>
+    </div>
+    
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Contacts App!",
+      contacts: [],
+    };
+  },
+  created: function () {
+    this.contactsIndex();
+  },
+  methods: {
+    contactsIndex: function () {
+      console.log("in contacts index");
+      axios.get("http://localhost:3000/api/contacts").then((response) => {
+        console.log(response.data);
+        this.contacts = response.data;
+      });
+    },
+  },
+};
 </script>
